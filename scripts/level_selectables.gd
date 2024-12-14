c14aec623708e40515bafd90444419aaea1a5448
NhgrtPlayer <nhgrtplayer@hotmail.com>
2024-12-14 11:41:21 +0100

    models and select logic updated

--- scripts/level_selectables.gd
+++ scripts/level_selectables.gd
@@ -4,21 +4,36 @@ var selectable_model: PackedScene
 var dragged_instance = null
 var drag_plane = Plane(Vector3.UP, 1)
 
+func start_dragging(instance):
+	add_child(dragged_instance)
+	var mouse_position_3d = get_mouse_position_on_plane()
+	if mouse_position_3d:
+		dragged_instance.global_transform.origin = mouse_position_3d
+
+func stop_dragging():
+	dragged_instance = null
+	selectable_model = null
+
+func cancel_dragging():
+	if is_instance_valid(dragged_instance):
+		dragged_instance.queue_free()
+	dragged_instance = null
+	selectable_model = null
+
+func toggle_dragging():
+	if dragged_instance == null:
+		dragged_instance = selectable_model.instantiate()
+		start_dragging(dragged_instance)
+	else:
+		stop_dragging()
+
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT and selectable_model:
-			if dragged_instance == null:
-				# Charger la scène
-				# Instancier la scène
-				dragged_instance = selectable_model.instantiate()
-				add_child(dragged_instance)
-				# Initialiser la position de l'instance sur le plan
-				var mouse_position_3d = get_mouse_position_on_plane()
-				if mouse_position_3d:
-					dragged_instance.global_transform.origin = mouse_position_3d
-			else:
-				# Relâcher l'objet
-				dragged_instance = null
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT and selectable_model:
+				toggle_dragging()
+			if event.button_index == MOUSE_BUTTON_RIGHT:
+				cancel_dragging()
 	elif event is InputEventMouseMotion and dragged_instance:
 		# Mettre à jour la position de l'instance pour suivre la souris sur le plan
 		var mouse_position_3d = get_mouse_position_on_plane()
@@ -26,17 +41,16 @@ func _input(event):
 			dragged_instance.global_transform.origin = mouse_position_3d
 
 func get_mouse_position_on_plane():
-	# Récupérer la caméra
 	var camera = get_viewport().get_camera_3d()
 	if camera:
-		# Obtenir le rayon projeté par la souris
 		var ray_origin = camera.project_ray_origin(get_viewport().get_mouse_position())
 		var ray_direction = camera.project_ray_normal(get_viewport().get_mouse_position())
-		# Calculer l'intersection du rayon avec le plan
 		var intersection = drag_plane.intersects_ray(ray_origin, ray_direction)
 		return intersection
 	return null
 
-
 func _on_game_ui_selectable_model_selected(passed_selectable_model):
 	selectable_model = passed_selectable_model
+	if selectable_model:
+		dragged_instance = selectable_model.instantiate()
+		start_dragging(dragged_instance)
--- scripts/selectables.gd
+++ scripts/selectables.gd
@@ -2,5 +2,5 @@ extends PanelContainer
 
 signal selectable_model_selected
 
-func _on_selectable_selected(selectable_name, selectable_model):
+func _on_selectable_selected(_selectable_name, selectable_model):
 	selectable_model_selected.emit(selectable_model)
